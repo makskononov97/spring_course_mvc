3a8e8aa7324e49fe336e81d0c95eefc78d2a7447
makskononov97 <115967472+makskononov97@users.noreply.github.com>
2022-10-24 15:51:31 +0700

    Initial commit

---
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -0,0 +1,41 @@
+package com.zaurtregulov.spring.mvc;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/employee")
+public class MyController {
+
+    @RequestMapping("/")
+    public String showFirstView(){
+        return "first-view";
+    }
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails(Model model){
+
+//        Employee employee = new Employee();
+//        employee.setName("Ivan");
+//        employee.setSurname("Petrov");
+//        employee.setSalary(500);
+
+        model.addAttribute("employee", new Employee());
+
+        return "ask-emp-details-view";}
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee employee
+            , BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "ask-emp-details-view";
+        }
+        else {
+        return "show-emp-details-view";}
+    }
+}
